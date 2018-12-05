f2e87aa353b3c2ed58c758902b3116273fa4c6da
Omar Lakrouz <omarlkrz1@gmail.com>
2018-12-04 19:42:42 -0500

    sixth commit

---
+++ WebApi10/ModelLocal/UrlWithParam.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inventV2.ModelLocal
+{
+    public class UrlWithParam
+    {
+        public String Url { get; set; }
+        public List<String> ListParam { get; set; }
+
+        public UrlWithParam(string url, List<string> listParam)
+        {
+            Url = url;
+            ListParam = listParam;
+        }
+        public UrlWithParam()
+        {
+          
+        }
+    }
+}
---
+++ WebApi10/ModelLocal/UserLogin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inventV2.ModelLocal
+{
+    public class UserLogin
+    {
+        public string IdUser { get; set; }
+        public string Password { get; set; }
+
+        public UserLogin(string idUser, string password)
+        {
+            IdUser= idUser;
+            Password = password;
+        }
+        public UserLogin()
+        {
+           
+        }
+    }
+}
